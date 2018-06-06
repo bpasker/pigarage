ea02341f67016e91e792d46671cc063d67cbc427
bpasker <brandon@bpasker.com>
2018-06-05 22:56:39 -0500

    add setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get install -y  python-pip
+sudo apt-get install -y supervisor
+sudo apt-get install -y sqlite3
+sudo pip install -y pyopenssl
+sudo pip install -y flask 
+sudo pip install -y SQLAlchemy
+sudo pip install -y Flask-HTTPAuth
+sudo pip install -y passlib[bcrypt]
