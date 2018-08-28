3aecd2022432852ba3bb0af169f79c73c473f2d8
Brandon Asker <brandon@bpasker.com>
2018-08-27 21:15:16 -0500

    json

--- pigarage.py
+++ pigarage.py
@@ -175,7 +175,7 @@ def new_user():
     #Commit to DB
     db.session.commit()
 
-    return (jsonify({['username': user.username]}), 201,
+    return (jsonify({{'username': user.username}}), 201,
             {{'Location': url_for('get_user', id=user.id, _external=True)}})
 
 
