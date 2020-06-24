446407eaf5c9c3958d032ecc294840eea86a3ef8
Brandon Asker <brandon@bpasker.com>
2020-06-23 19:26:51 -0500

    clean setup to persist keys

--- entrySetup.sh
+++ entrySetup.sh
@@ -14,6 +14,16 @@ else
     cp /pigarage/default/defaultSettings.cfg /settings/settings.cfg
 fi
 
+# letsencrypt Path
+letsencryptFolder=/settings/letsencrypt
+
+# Check if letsencrypt folder is present
+if [ -d "$letsencryptFolder" ]; then
+    echo "$letsencryptFolder exists."
+else
+    mkdir /settings/letsencrypt
+fi
+
 # Create default DB with default user
 defaultDB=/settings/pigarage.db
 
@@ -26,16 +36,37 @@ fi
 # Replace your domain the the flask settings
 sed -i "s/replaceme/$myDomain/g" /etc/nginx/sites-enabled/flask_settings
 
-# Create temp cert for SSL so it starts
-mkdir /etc/letsencrypt/temp
-openssl req -subj "/CN=$myDomain/O=$myDomain/C=US" -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout /etc/letsencrypt/temp/privkey.pem -out /etc/letsencrypt/temp/fullchain.cert
+# letsencrypt keyfolder
+letsencryptKey=/settings/letsencrypt/keys
 
-# Start nginx for ingress
-service nginx start
+# Check if letsencrypt folder is present
+if [ -d "$letsencryptKey" ]; then
+    echo "$letsencryptKey exists."
+else
+    #If no key is present the clean the old directory and create symlink to settings folder
+    rm -rf /etc/letsencrypt/
+    ln -s /settings/letsencrypt /etc/letsencrypt/
+fi
 
-# Enable real SSL certs via https://letsencrypt.org/
-if $myCertBot ; then
-    certbot --nginx --agree-tos --no-redirect --no-eff-email -m $myEmail -d $myDomain
+# Check if letsencrypt folder is present
+if [ -d "$letsencryptKey" ]; then
+    echo "$letsencryptKey exists."
+
+    # Start nginx for ingress
+    service nginx start
+else
+
+    # Create temp cert for SSL so it starts
+    mkdir /etc/letsencrypt/temp
+    openssl req -subj "/CN=$myDomain/O=$myDomain/C=US" -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout /etc/letsencrypt/temp/privkey.pem -out /etc/letsencrypt/temp/fullchain.cert
+
+    # Start nginx for ingress
+    service nginx start
+
+    # Enable real SSL certs via https://letsencrypt.org/
+    if $myCertBot ; then
+        certbot --nginx --agree-tos --no-redirect --no-eff-email -m $myEmail -d $myDomain
+    fi
 fi
 
 # Start Supervisor to enable pigarage
